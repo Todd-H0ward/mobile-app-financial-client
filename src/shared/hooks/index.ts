0c6584af6ad22c172907e51832e81d79e67bf8e9
Denis Rudenko <dr144371@gmail.com>
2026-09-15 00:50:32 +0300

    Add base hooks

---
+++ src/shared/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useAppLanguage } from './use-app-language';
+export { useColorScheme } from './use-color-scheme';
+export { useDebounce } from './use-debounce';
+export { useBreakpoint, useResponsive } from './use-responsive';
+export { useTheme } from './use-theme';
