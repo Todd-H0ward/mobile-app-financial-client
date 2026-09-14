27968fd5ad8d77b570263039170b8cf43f29b92c
Denis Rudenko <dr144371@gmail.com>
2026-09-15 00:57:08 +0300

    Add base constants and styles

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,14 @@
+export { APP_VERSION } from './app-version';
+export { STORAGE_KEYS } from './storage-keys';
+export type { Spacing, ThemeColor } from './theme';
+export {
+  BOTTOM_TAB_INSET,
+  COLORS,
+  CONTENT_PADDING,
+  FONTS,
+  HIT_SLOP_SIZE,
+  MAX_CONTENT_WIDTH,
+  RADII,
+  SPACING,
+  SPACING_BASE,
+} from './theme';
